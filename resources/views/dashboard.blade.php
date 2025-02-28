cb45a6cb54bb3e2e07665374e93deb27d09750a9
HumaCode <humaidizakaria1998@gmail.com>
2025-02-28 21:21:35 +0700

    Integrasi Admin template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,81 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+<x-master-layout>
+
+    <x-slot name="toolbar">
+
+        <div id="kt_toolbar_container" class="container-xxl d-flex flex-stack flex-wrap gap-2">
+            <!--begin::Page title-->
+            <div class="page-title d-flex flex-column align-items-start me-3 py-2 py-lg-0 gap-2">
+                <!--begin::Title-->
+                <h1 class="d-flex text-gray-900 fw-bold m-0 fs-3">Dashboard</h1>
+                <!--end::Title-->
+                <!--begin::Breadcrumb-->
+                {{-- <ul class="breadcrumb breadcrumb-dot fw-semibold text-gray-600 fs-7">
+                    <!--begin::Item-->
+                    <li class="breadcrumb-item text-gray-600">
+                        <a href="index.html" class="text-gray-600 text-hover-primary">Home</a>
+                    </li>
+                    <!--end::Item-->
+                    <li class="breadcrumb-item text-gray-600">Utilities</li>
+                    <!--end::Item-->
+                    <!--begin::Item-->
+                    <li class="breadcrumb-item text-gray-600">Modals</li>
+                    <!--end::Item-->
+                    <!--begin::Item-->
+                    <li class="breadcrumb-item text-gray-600">General</li>
+                    <!--end::Item-->
+                    <!--begin::Item-->
+                    <li class="breadcrumb-item text-gray-500">View Users</li>
+                    <!--end::Item-->
+                </ul> --}}
+                <!--end::Breadcrumb-->
+            </div>
+            <!--end::Page title-->
+            <!--begin::Actions-->
+            {{-- <div class="d-flex align-items-center">
+                <!--begin::Button-->
+                <a href="#" class="btn btn-icon btn-color-primary bg-body w-35px h-35px w-lg-40px h-lg-40px me-3"
+                    data-bs-toggle="modal" data-bs-target="#kt_modal_upgrade_plan">
+                    <i class="ki-duotone ki-file-added fs-2 fs-md-1">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                    </i>
+                </a>
+                <!--end::Button-->
+
+            </div> --}}
+            <!--end::Actions-->
+        </div>
+
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    {{-- content --}}
+    <div class="card">
+        <!--begin::Card body-->
+        <div class="card-body">
+            <!--begin::Heading-->
+            <div class="card-px text-center pt-15 pb-15">
+                <!--begin::Title-->
+                <h2 class="fs-2x fw-bold mb-0">Selamat Datang</h2>
+                <!--end::Title-->
+                <!--begin::Description-->
+                <p class="text-gray-500 fs-4 fw-semibold py-7">Click on the below buttons to
+                    launch
+                    <br />user lists example.
+                </p>
+                <!--end::Description-->
+                <!--begin::Action-->
+                <a href="#" class="btn btn-primary er fs-6 px-8 py-4" data-bs-toggle="modal"
+                    data-bs-target="#kt_modal_view_users">View Users</a>
+                <!--end::Action-->
             </div>
+            <!--end::Heading-->
+            <!--begin::Illustration-->
+            <div class="text-center pb-15 px-5">
+                <img src="{{ asset('/') }}backend/assets/media/illustrations/sketchy-1/2.png" alt=""
+                    class="mw-100 h-200px h-sm-325px" />
+            </div>
+            <!--end::Illustration-->
         </div>
+        <!--end::Card body-->
     </div>
-</x-app-layout>
+</x-master-layout>
